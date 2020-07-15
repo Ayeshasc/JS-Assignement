534f377c44d3e86c7978ce8aeba6318bb6e54e98
Ayeshasc <67307999+Ayeshasc@users.noreply.github.com>
2020-07-15 19:12:47 +0530

    Assignement1_day2

---
+++ assignement1_day2.js
@@ -0,0 +1,10 @@
+/* Assignement 2  js Day 2    Name : Ayesha Sattar Choudhary 
+1.write a js code which takes input from the user and logs it in the console
+*/
+console.log("hello");
+let age = prompt("What is your age");
+ console.log(age);
+
+
+
+ 
