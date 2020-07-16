5c8d6492eeb79f3ffb51bc5e6f4104fee44adb25
Ayeshasc <67307999+Ayeshasc@users.noreply.github.com>
2020-07-16 20:11:20 +0530

    Day 3 Assignement 3

---
+++ third.js
@@ -0,0 +1,66 @@
+/* Name = Ayesha Sattar Choudhary js batch 01
+
+Assignement 3
+take marks as input and ang grade him accordingly 
+
+*/
+
+console.log("third assignment")
+
+let marks = prompt("Enter the Marks");
+ console.log(marks);
+if(marks == 50 ){
+         console.log("marks are 50 and grade is B");
+         }
+     else if(marks>=50){
+         console.log("marks are grater than 50  and grade is A");
+     }
+     else{
+         console.log("Fail")
+     } 
+
+
+
+
+/* code using ternarry
+
+
+     console.log("third assignment")
+
+     let marks = prompt("Enter the Marks");
+      console.log(marks);
+
+
+ let result = (marks =50) ? 'marks are 50 and grade is B': 
+             (marks>=20) ? 'marks are grater than 50  and grade is A':
+          '"Fail';
+
+ console.log(result);*/
+
+
+
+
+ /*code using switch case
+ 
+
+ 
+console.log("third assignment")
+
+     let marks = prompt("Enter the Marks");
+      console.log(marks)
+
+switch (marks) {
+    case 50:
+        console.log("marks are 50 and grade is B");
+        break;
+    case marks >= 50:
+    case 21: 
+        console.log('marks are grater than 50  and grade is A');
+        break;
+    default:
+        console.log('Fail');
+        */
+
+
+
+
