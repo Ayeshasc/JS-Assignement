c0b4e40650c2bc244ed65027aab276f278c92546
Ayeshasc <67307999+Ayeshasc@users.noreply.github.com>
2020-07-16 20:09:59 +0530

    Day 3 Assignement 1

---
+++ one.js
@@ -0,0 +1,20 @@
+
+/* Name = Ayesha Sattar Choudhary js batch 01
+
+Assignement 1
+take number from user and print number is even or odd
+
+*/
+
+console.log("Assignement one")
+let num = prompt("Enter the number");
+ console.log(num);
+
+
+ if(num %2==0){
+        console.log("The Number enter is", num ,"the number is even");
+     }
+     else{
+        console.log("The Number enter is", num ,"the number is odd");
+    }
+
