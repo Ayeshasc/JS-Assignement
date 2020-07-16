1d6b976eef681e3aeb696e780e37b50b04a703f7
Ayeshasc <67307999+Ayeshasc@users.noreply.github.com>
2020-07-16 20:10:52 +0530

    Day 3 Assignement 2

---
+++ second.js
@@ -0,0 +1,9 @@
+/*  Name = Ayesha Sattar Choudhary js batch 01
+Assignement 2 Day 3
+2. write a program which take os name and version from user and print it on console
+*/
+
+console.log("Assignement second")
+let num = prompt("Enter the Os name and the version ");
+ console.log(num);
+console.log("The OS name and version is",num)
